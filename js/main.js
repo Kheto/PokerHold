6139cf2d43f27387b44ae4f79cbc8027e14e26a0
kheto <k@viru.us>
2019-10-27 18:38:28 +0000

    instructions

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@ var deck = [];
 var SUITS = ["c", "d", "h", "s"],
   VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+var SCORE_VALUES = {
+  "High Card": 0,
+  Pair: 1,
+  "Two Pair": 5,
+  "Three of a Kind": 20,
+  "Full House": 50,
+  Flush: 100,
+  Straight: 125,
+  "Four of a Kind": 300
+};
 var FLOP_SIZE = 5;
 var HAND_SIZE = 3;
 
@@ -9,7 +19,7 @@ var score = {},
   tableCards = [],
   carriedCard = [];
 
-  var animTime = 1;
+var animTime = 1;
 
 deck.populate = function() {
   for (suit of SUITS) {
@@ -95,43 +105,41 @@ function setupRound() {
     }
     tableCards.push(newCard);
     var cardEl = createCard(newCard[0], newCard[1]);
-    if(isCarried){
-        cardEl.style.animation = "none";
-        cardEl.style.animationDuration = "0";
+    if (isCarried) {
+      cardEl.style.animation = "none";
+      cardEl.style.animationDuration = "0";
     }
-    document
-      .querySelector(".hand-container")
-      .appendChild(cardEl);
+    document.querySelector(".hand-container").appendChild(cardEl);
   }
 }
 
 function cleanup() {
   tableCards = [];
   animTime = 0.25;
-  removeChildren(document.querySelector(".hand-container"))
-  removeChildren(document.querySelector(".flop-container"))
+  removeChildren(document.querySelector(".hand-container"));
+  removeChildren(document.querySelector(".flop-container"));
 }
 
 function updateScore() {
-    const scoreboard = 
-    document.querySelector(".scoreboard");
-    removeChildren(scoreboard);
-    
-    for(row in score){
-        var newRow = document.createElement("li");
-        newRow.innerText = `${row}: ${score[row]}`;
-        scoreboard.appendChild(newRow);
-    }
-    newRow = document.createElement("li");
-    newRow.innerText = `Cards in deck: ${deck.length}`;
+  const scoreboard = document.querySelector(".scoreboard");
+  removeChildren(scoreboard);
+  var total = 0;
+
+  for (row in score) {
+    var newRow = document.createElement("li");
+    newRow.innerText = `${row}: ${score[row]} x $${SCORE_VALUES[row]}`;
     scoreboard.appendChild(newRow);
+    total += score[row] * SCORE_VALUES[row];
+  }
+  newRow = document.createElement("li");
+  newRow.innerText = `Total: $${total}`;
+  scoreboard.appendChild(newRow);
 }
 
-function removeChildren(element){
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
-
+function removeChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
 }
 
 setupRound();
