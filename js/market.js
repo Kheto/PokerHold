a76e54b21887382efda9adfe7b982fab84fb81b0
kheto <k@viru.us>
2019-11-03 22:59:55 +0000

    fixed recurring items and added more

--- js/market.js
+++ js/market.js
@@ -6,7 +6,7 @@ var items = [
   {
     id: 0,
     dependsOn: -1,
-    price: 4000,
+    price: 2000,
     desc: "Take out a hit on Jeremy Clarkson.",
     result: `Jeremy Clarkson saunters confidently to his car and gets in. After
   briefly reveling in how rich he is, he turns the key in the ignition and
@@ -32,16 +32,57 @@ var items = [
     id: 3,
     dependsOn: 0,
     price: 10000,
-    desc: "Buy Jeremy's old cars",
-    result: ""
+    desc: "Jeremy's old cars",
+    result:
+      "You've bought all of Jeremy Clarkson's old cars. At the auction his wife seems distraught, you comfort her. A BBC executive offers you his old job."
   },
   {
     id: 4,
     dependsOn: 1,
     price: 70,
-    desc: "Buy another video game",
+    desc: "Another video game",
     result:
       "Something about this game's story speaks to you deeply. You spend the weekend explaining it to your friends. After a week you have stopped playing without completing it. One day you'll try to play it again but you won't remember the controls."
+  },
+  {
+    id: 5,
+    dependsOn: 3,
+    price: 300,
+    desc: "Monthly train ticket to the BBC studio",
+    result:
+      "You are now a co-host on Jeremy Clarkson's old TV programme. While wrapping up the last episode of the series you run into his wife again. She asks you to dinner."
+  },
+  {
+    id: 6,
+    dependsOn: 5,
+    price: 200,
+    desc:
+      "Take Jeremy Clarson's widow to dinner and assume your place within his family.",
+    result: "You are Jeremy Clarkson"
+  },
+  {
+    id: 7,
+    dependsOn: 2,
+    price: 60,
+    desc: "Box of old CDs",
+    result:
+      "None of these are good. You listen to the one with the highest rating on Last.fm out of stubbornness."
+  },
+  {
+    id: 8,
+    dependsOn: -1,
+    price: 5,
+    desc: "Extra hot chilli sauce",
+    result:
+      "You pour some gingerly onto a Dorito. The taste is pleasant for a few seconds until you are overcome with pain. This was a bad idea."
+  },
+  {
+    id: 9,
+    dependsOn: 2,
+    price: 600,
+    desc: "Used car",
+    result:
+      "This car is only slightly nicer than you previous car. It makes fewer weird noises. You realise you left your canvas bag in the old one."
   }
 ];
 
@@ -54,12 +95,15 @@ var cash = getCash();
 function populate() {
   var productList = document.querySelector(".product-list");
   var history = getPurchaseHistory();
+  if (history.length >= items.length){
+    items = [{id: 9999 + history.length, dependsOn: -1, price: 99999, desc: "Game over, idiot. You fool. You clown. Go do something important.", result: "Stupid idiot"}]
+  }
 
   for (let item of items) {
     console.log(item.id, item.dependsOn);
     if (
       (!history.contains(item.id) && item.dependsOn < 0) ||
-      history.contains(item.dependsOn)
+      (history.contains(item.dependsOn) && !history.contains(item.id))
     ) {
       var rowElement = createProductRow(item);
       rowElement.id = item.id;
